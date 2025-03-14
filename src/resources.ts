a2e30eb747235bd8d2c12a7c07097a5d4a8a4cd7
xyugen <alex.arias.jorym@gmail.com>
2025-03-14 21:12:30 +0800

    feat: load women sprites resource

--- src/resources.ts
+++ src/resources.ts
@@ -8,6 +8,27 @@ export const Resources = {
     background: new ImageSource("./sprites/backgrounds/main-bg.png"),
     // tilemap: new ImageSource("./sprites/tileset-1.png"),
   },
+  womenSprites: {
+    "julia-celeste-smith": new ImageSource(
+      "./images/babae/julia-celeste-smith.png"
+    ),
+    "segunda-katigbak": new ImageSource("./images/babae/segunda-katigbak.png"),
+    "leonor-valenzuela": new ImageSource(
+      "./images/babae/leonor-valenzuela.png"
+    ),
+    "leonor-rivera": new ImageSource("./images/babae/leonor-rivera.png"),
+    "consuelo-ortiga": new ImageSource("./images/babae/consuelo-ortiga.png"),
+    "o-sei-san": new ImageSource("./images/babae/o-sei-san.png"),
+    "gertrude-beckett": new ImageSource("./images/babae/gertrude-beckett.png"),
+    "nelly-boustead": new ImageSource("./images/babae/nelly-boustead.png"),
+    "suzanne-jacoby": new ImageSource("./images/babae/suzanne-jacoby.png"),
+    "pastora-necesario": new ImageSource(
+      "./images/babae/pastora-necesario.png"
+    ),
+    "josephine-bracken": new ImageSource(
+      "./images/babae/josephine-bracken.png"
+    ),
+  },
   fonts: {
     pixel: new FontSource("./fonts/silkscreen-regular.woff2", "SilkScreen", {
       filtering: ImageFiltering.Pixel,
